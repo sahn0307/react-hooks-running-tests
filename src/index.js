c56f73b15ac4631e5ce50fcb8a754aaf340facf0
Sung-Jin Ahn <sahn0307@gmail.com>
2024-02-19 11:54:57 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+function Header(props) {
+    return <h1>hello from the Header!</h1>;
+  }
+  
+  export default Header;
